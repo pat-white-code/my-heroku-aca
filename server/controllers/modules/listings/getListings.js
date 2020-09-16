ff3a45e17f3106efc71b1ae98239ab1f6968657f
pat-white-code <pat.k.white@gmail.com>
2020-09-16 14:12:06 -0500

    gets listings

--- server/controllers/modules/listings/getListings.js
+++ server/controllers/modules/listings/getListings.js
@@ -1,19 +1,33 @@
 
-const connection = require('../../../connection');
+// const connection = require('../../../connection');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const getListings = (req, res) => {
-  const client = connection();
-  const {bedrooms_gte, bathrooms_gte, amenities} = req.query;
-  let bedroomsFilter = {};
-  if(bedrooms_gte) {
-    bedroomsFilter.$gte = bedrooms_gte
-  }
+
+const getListings = async (req, res) => {
+  const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  // const {bedrooms_gte, bathrooms_gte, amenities} = req.query;
+  // let bedroomsFilter = {};
+  // if(bedrooms_gte) {
+  //   bedroomsFilter.$gte = bedrooms_gte
+  // }
   
-  let filter = {
-    bedrooms: bedroomsFilter
+  // let filter = {
+  //   bedrooms: bedroomsFilter
+  // }
+
+  try{
+    await client.connect();
+    const cursor = client.db("sample_airbnb").collection("listingsAndReviews")
+      .find()
+      .limit(50);
+
+    const results = await cursor.toArray();
+    res.json(results);
+  } catch(e) {
+    res.status(500).send(e)
   }
-  
-  res.send(req.query);
 }
 
 module.exports = getListings;
