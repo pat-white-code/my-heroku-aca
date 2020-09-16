aeaa163101017e969a7258937d82b1c2e4029854
pat-white-code <pat.k.white@gmail.com>
2020-09-16 11:52:10 -0500

    starts test folders

---
+++ server/test/connection.test.js
@@ -0,0 +1,9 @@
+// import { TestScheduler } from 'jest';
+// import connection from '../controllers/connection';
+
+
+// describe("connection", () => {
+//   test('connects successfully', ()=> {
+
+//   })
+// })
