2a507e03980212e0291ab91d5a6d0c3aafebd043
pat-white-code <pat.k.white@gmail.com>
2020-09-16 12:21:42 -0500

    creates post controller with mongodb

---
+++ server/controllers/createListing.js
@@ -0,0 +1,36 @@
+// async function createListing(client, newListing){
+//   const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+//   console.log(`New listing created with the following id: ${result.insertedId}`);
+// }
+
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
+
+// const createListing = async (client, newListing) => {
+//   const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+
+//   console.log(`New listing created with the following id: ${result.insertedId}`)
+// }
+
+const main = async () => {
+  const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri);
+
+  let listing = {
+    name: "Lovely Loft",
+    summary: "A charming loft in Paris",
+    bedrooms: 1,
+    bathrooms: 1
+  }
+
+  try{
+    await client.connect();
+    const abnb = client.db("sample_airbnb").collection("listingsAndReviews");
+    let result = await abnb.insertOne(listing);
+    console.log(`New listing created with the following id: ${result.insertedId}`)
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+main().catch(console.error);
