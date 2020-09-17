920a633e935e09275a0e3401d0b9aff4b81be1e1
pat-white-code <pat.k.white@gmail.com>
2020-09-17 11:36:20 -0500

    refactors query parameters

--- server/controllers/modules/listings/getListings.js
+++ server/controllers/modules/listings/getListings.js
@@ -1,27 +1,27 @@
 
+const { expect } = require('chai');
 const connection = require('../../../connection');
 
 
 const getListings = async (req, res) => {
   const client = connection();
-  const {bedrooms_gte=0, bathrooms_gte=0} = req.query;
+  const {bedrooms_gte=0, bathrooms_gte, reviews, images} = req.query;
   // let bedroomsFilter = {};
-
   let filter = {
-    bedrooms: {
-      $gte: parseInt(bedrooms_gte),
-    },
-    bathrooms: {
-      $gte: parseInt(bathrooms_gte)
-    }
   }
+
+  bedrooms_gte && (filter.bedrooms = {...filter.bedrooms, $gte: parseInt(bedrooms_gte)});
+  bathrooms_gte && (filter.bathrooms = {...filter.bathrooms, $gte: parseInt(bathrooms_gte)});
+  reviews && (filter.reviews = {...filter.reviews, $exists: true});
+  images && (filter.images = {...filter.images, $exists: true});
+
 
   try{
     await client.connect();
     const cursor = client.db("sample_airbnb").collection("listingsAndReviews")
       .find(filter)
       .limit(50)
-      .project({_id: 0, bedrooms: 1, bathrooms: 1})
+      .project({_id: 0, bedrooms: 1, bathrooms: 1, reviews:1, images: 1})
 
     const results = await cursor.toArray();
     res.json(results);
